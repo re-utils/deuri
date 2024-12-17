c2e38bb1ff78b3e3da1dd44bdf7c2d00e81d6dda
aquapi <aquaplmc@gmail.com>
2024-12-17 12:00:20 +0700

    Make implementation slightly faster

--- bench/valid.bench.ts
+++ bench/valid.bench.ts
@@ -1,14 +1,29 @@
 import { summary, run, bench } from 'mitata';
-import { decode } from '../lib/index.js';
+import { decode, decodeSegment } from '../lib/index.js';
 // @ts-expect-error Stfu
 import fastDecodeURIComponent from 'fast-decode-uri-component';
 import data from '../suites/valid.json';
 
-// Example benchmark
+// No substring
 summary(() => {
   bench('deuri', () => data.map(decode));
   bench('fast-decode-uri-component', () => data.map(fastDecodeURIComponent));
-  bench('Native', () => data.map(decodeURIComponent));
+  bench('native', () => data.map(decodeURIComponent));
+});
+
+// With substring
+summary(() => {
+  // Test this for query parsers
+  const substringData = data.filter((str) => str.includes('?'));
+
+  // eslint-disable-next-line
+  const fastDecodeURIComponentSubstring = (str: string): string | null => fastDecodeURIComponent(str.substring(0, str.indexOf('?')));
+  const nativeDecodeURIComponentSubstring = (str: string): string | null => decodeURIComponent(str.substring(0, str.indexOf('?')));
+  const deuriSubstring = (str: string): string | null => decodeSegment(str, 0, str.indexOf('?'));
+
+  bench('substring - deuri', () => substringData.map(deuriSubstring));
+  bench('substring - fast-decode-uri-component', () => substringData.map(fastDecodeURIComponentSubstring));
+  bench('substring - native', () => substringData.map(nativeDecodeURIComponentSubstring));
 });
 
 // Start the benchmark
--- src/index.ts
+++ src/index.ts
@@ -1,491 +1,44 @@
+/* eslint-disable */
 const hexMap = [
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-  255,
-
-  10,
-  11,
-  12,
-  13,
-  14,
-  15
+  ...new Array(48).fill(255),
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, // 0...9 (index 48 - 57)
+  ...new Array(7).fill(255),
+  10, 11, 12, 13, 14, 15, // A - F (index 65 - 70)
+  ...new Array(26).fill(255),
+  10, 11, 12, 13, 14, 15 // a - f (index 97 - 102)
 ];
 
-const data = [
-  // The first part of the table maps bytes to character to a transition.
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  1,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  2,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  3,
-  4,
-  4,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  5,
-  6,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  8,
-  7,
-  7,
-  10,
-  9,
-  9,
-  9,
-  11,
-  4,
-  4,
-  4,
-  4,
-  4,
-  4,
-  4,
-  4,
-  4,
-  4,
-  4,
-
-  // The second part of the table maps a state to a new state when adding a transition.
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  268,
-  256,
-  256,
-  256,
-  256,
-  280,
-  292,
-  304,
-  316,
-  328,
-  340,
-  352,
-  256,
-  268,
-  268,
-  268,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  280,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  280,
-  280,
-  280,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  280,
-  280,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  304,
-  304,
-  304,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  304,
-  304,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  304,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256,
-  256
-];
-
-// Maps the current transition to a mask that needs to apply to the byte.
-const mask = [0x7F, 0x3F, 0x3F, 0x3F, 0x00, 0x1F, 0x0F, 0x0F, 0x0F, 0x07, 0x07, 0x07];
-
-// eslint-disable-next-line
 const highHex = (code: number): number => code > 102 ? 255 : hexMap[code] << 4;
-// eslint-disable-next-line
 const lowHex = (code: number): number => code > 102 ? 255 : hexMap[code];
+
+// Map bytes to character to a transition
+const transitionType = [
+  ...new Array(128).fill(0),
+  ...new Array(16).fill(1),
+  ...new Array(16).fill(2),
+  ...new Array(32).fill(3),
+
+  4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+  6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 7, 7,
+  10, 9, 9, 9, 11, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+];
+
+// Maps a state to a new state when adding a transition
+const nextState = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  12, 0, 0, 0, 0, 24, 36, 48, 60, 72, 84, 96,
+  0, 12, 12, 12, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 24, 24, 24, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 24, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 48, 48, 48, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 48, 48, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+
+// Maps the current transition to a mask that needs to apply to the byte
+const mask = [0x7F, 0x3F, 0x3F, 0x3F, 0x00, 0x1F, 0x0F, 0x0F, 0x0F, 0x07, 0x07, 0x07];
 
 /**
  * Decode the full string
@@ -494,30 +47,31 @@ export const decode = (url: string): string | null => {
   let percentPosition = url.indexOf('%');
   if (percentPosition === -1) return url;
 
-  // eslint-disable-next-line
-  let end = url.length;
-  if (percentPosition + 3 > end) return null;
+  // Ensure percentPosition always has 2 chars after
+  let endSub3 = url.length - 3;
+  if (percentPosition > endSub3) return null;
 
-  // eslint-disable-next-line
   let decoded = '',
     last = 0,
     codepoint = 0,
     startOfOctets = percentPosition,
-    state = 268,
+    // UTF_ACCEPT
+    state = 12,
     byte: number,
-    dat: number;
+    type: number;
 
-  while (percentPosition < end) {
+  while (true) {
     byte = highHex(url.charCodeAt(percentPosition + 1)) | lowHex(url.charCodeAt(percentPosition + 2));
-    dat = data[byte];
+    type = transitionType[byte];
 
-    state = data[state + dat];
-    if (state === 256) return null;
+    state = nextState[state + type];
+    // UTF_REJECT
+    if (state === 0) return null;
 
-    // eslint-disable-next-line
-    codepoint = codepoint << 6 | byte & mask[dat];
+    codepoint = codepoint << 6 | byte & mask[type];
 
-    if (state === 268) {
+    // UTF_ACCEPT
+    if (state === 12) {
       decoded += url.substring(last, startOfOctets);
       decoded += codepoint > 0xFFFF
         ? String.fromCharCode(
@@ -526,21 +80,25 @@ export const decode = (url: string): string | null => {
         )
         : String.fromCharCode(codepoint);
 
+      // Search next encoded component
       last = percentPosition + 3;
 
       percentPosition = url.indexOf('%', last);
       if (percentPosition === -1)
         return decoded + url.substring(last);
 
+      // Ensure percentPosition always has 2 chars after
+      if (percentPosition > endSub3)
+        return null;
+
       startOfOctets = percentPosition;
       codepoint = 0;
     } else {
+      // Search for next %
+      if (percentPosition >= endSub3 || url.charCodeAt(percentPosition + 3) !== 37) return null;
       percentPosition += 3;
-      if (percentPosition >= end || url.charCodeAt(percentPosition) !== 37) return null;
     }
   }
-
-  return decoded + url.substring(last);
 };
 
 /**
@@ -549,27 +107,32 @@ export const decode = (url: string): string | null => {
 export const decodeSegment = (url: string, start: number, end: number): string | null => {
   let percentPosition = url.indexOf('%', start);
   if (percentPosition === -1) return url;
-  if (percentPosition + 3 > end) return null;
 
-  // eslint-disable-next-line
+  // Ensure percentPosition always has 2 chars after
+  end -= 3;
+  if (percentPosition > end) return null;
+
+  start = percentPosition;
   let decoded = '',
     codepoint = 0,
     startOfOctets = percentPosition,
-    state = 268,
+    // UTF_ACCEPT
+    state = 12,
     byte: number,
-    dat: number;
+    type: number;
 
-  while (percentPosition < end) {
+  while (true) {
     byte = highHex(url.charCodeAt(percentPosition + 1)) | lowHex(url.charCodeAt(percentPosition + 2));
-    dat = data[byte];
+    type = transitionType[byte];
 
-    state = data[state + dat];
-    if (state === 256) return null;
+    state = nextState[state + type];
+    // UTF_REJECT
+    if (state === 0) return null;
 
-    // eslint-disable-next-line
-    codepoint = codepoint << 6 | byte & mask[dat];
+    codepoint = codepoint << 6 | byte & mask[type];
 
-    if (state === 268) {
+    // UTF_ACCEPT
+    if (state === 12) {
       decoded += url.substring(start, startOfOctets);
       decoded += codepoint > 0xFFFF
         ? String.fromCharCode(
@@ -578,19 +141,23 @@ export const decodeSegment = (url: string, start: number, end: number): string |
         )
         : String.fromCharCode(codepoint);
 
+      // Search next encoded component
       start = percentPosition + 3;
 
       percentPosition = url.indexOf('%', start);
       if (percentPosition === -1)
         return decoded + url.substring(start);
 
+      // Ensure percentPosition always has 2 chars after
+      if (percentPosition > end)
+        return null;
+
       startOfOctets = percentPosition;
       codepoint = 0;
     } else {
+      // Search for next %
+      if (percentPosition >= end || url.charCodeAt(percentPosition + 3) !== 37) return null;
       percentPosition += 3;
-      if (percentPosition >= end || url.charCodeAt(percentPosition) !== 37) return null;
     }
   }
-
-  return decoded + url.substring(start);
 };
